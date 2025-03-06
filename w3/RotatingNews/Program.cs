801449b876c4ae8dd8962476c2ea73ff53f9d47f
cyprich <cypooriginal@gmail.com>
2025-03-06 21:04:52 +0100

    w3 po cviku

---
+++ w3/RotatingNews/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace RotatingNews;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            if (args.Length < 3)
+            {
+                Console.Error.WriteLine("Nezadali ste 3 argumenty");
+                return;
+            }
+
+            string message = args[0];
+            int row = int.Parse(args[1]);
+            ConsoleColor color = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), args[2], true);
+
+            Console.ForegroundColor = color;
+            Console.CursorTop = row - 1;
+            Console.CursorVisible = false;
+
+            int index = Console.WindowWidth - 1;
+
+            while (true)
+            {
+                Console.CursorLeft = index;
+                Console.CursorTop = row;
+
+                Console.WriteLine(message);
+
+                index--;
+                if (index < 0)
+                {
+                    index = Console.WindowWidth - 1;
+                }
+
+                Thread.Sleep(200);
+                /*await Task.Delay(1000);*/
+                Console.Clear();
+            }
+
+        }
+        finally
+        {
+            Console.ResetColor();
+            Console.CursorVisible = true;
+        }
+    }
+}
---
+++ w3/StatsApp/Program.cs
@@ -0,0 +1,53 @@
+﻿var file = File.ReadAllLines("txt/numbers.txt");
+int[] numbers = new int[file.Length];
+
+for (int i = 0; i < file.Length; i++)
+{
+    numbers[i] = int.Parse(file[i]);
+}
+
+void PrintStatistics(ReadOnlySpan<int> span)
+{
+    int sum = 0;
+
+    foreach (var i in span)
+    {
+        sum += i;
+    }
+
+    double mean = (double)sum / file.Length;
+
+    double variance = 0;
+
+    foreach (var i in span)
+    {
+        variance += Math.Pow(i - mean, 2);
+    }
+
+    variance /= file.Length;
+
+    System.Console.WriteLine($"\tSuma: {sum}");
+    System.Console.WriteLine($"\tPriemer: {mean}");
+    System.Console.WriteLine($"\tRozptyl: {variance}\n");
+}
+
+
+System.Console.WriteLine($"Prvy prvok: {file[0]}");
+System.Console.WriteLine($"Posledny prvok: {file[^1]}");
+System.Console.WriteLine($"Prostredny prvok: {file[file.Length / 2]}\n");
+
+PrintStatistics(numbers);
+
+var a = numbers.AsSpan();
+a[..300].Clear();
+System.Console.WriteLine("Prvych 300 prvkov = 0");
+PrintStatistics(a);
+
+a = numbers.AsSpan();
+a[4000..6001].Fill(500);
+System.Console.WriteLine("Prvky 4000 az 6000 = 500");
+PrintStatistics(a);
+
+a = numbers.AsSpan();
+System.Console.WriteLine("od indexu 5000");
+PrintStatistics(a[5000..]);
