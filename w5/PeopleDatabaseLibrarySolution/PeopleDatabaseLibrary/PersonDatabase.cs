118a6cd181933bfe75a65e1130b71ac6ca4e2c54
cyprich <cypooriginal@gmail.com>
2025-03-20 18:02:16 +0100

    w5 co sa stihlo

---
+++ w5/PeopleDatabaseLibrarySolution/PeopleDatabaseLibrary/PersonDatabase.cs
@@ -0,0 +1,78 @@
+﻿namespace PeopleDatabaseLibrary;
+
+// TODO 3.08: Vytvorte v programe inštanciu triedy PersonDatabase a pridajte do nej vygenerovanych 50 osob pomocou triedy PersonGenerator
+public class PersonDatabase
+{
+    private readonly List<Person> _people = [];
+
+    public void Add(Person person)
+    {
+        _people.Add(person);
+    }
+
+    public void Add(params Person[] people)
+    {
+        _people.AddRange(people);
+    }
+
+    public void Remove(Person person)
+    {
+        _people.Remove(person);
+    }
+
+    // TODO 3.09: Pridajte indexer na čítanie a aj zápis, ktorý vráti inštanciu triedy Person podľa indexu - poradia
+
+    public Person this[int index]
+    {
+        get { return _people[index]; }
+        set { _people[index] = value; }
+    }
+
+    // TODO 3.10: Otestujte get časť indexeru v programe, vráťte osobu na indexe 27
+    // TODO 3.11: Otestujte set časť indexeru v programe, zapíšte na index 27 novú osobu, ktorú vytvoríte
+
+
+    // TODO 3.12: Pridajte indexer len na čítanie, ktorý vráti prvú inštanciu triedy Person podľa mena a priezviska.
+
+    public Person? this[string firstName, string lastName]
+    {
+        get
+        {
+            List<Person> value = Find($"{firstName}{lastName}");
+            return value.Count > 0 ? value[0] : null;
+        }
+    }
+
+    // Ak taká nie je, vráti null hodnotu.
+    // TODO 3.13: Otestujte indexer v programe
+
+
+    public List<Person> Find(string text, Gender? gender = null)
+    {
+        var result = new List<Person>();
+
+        // Prejdeme vsetky polozky zoznamu a vyhladame v mene
+        foreach (var person in _people)
+        {
+            if (person.FullName.Contains(text))
+            {
+                if (gender == null || gender.Value == person.Gender)
+                    result.Add(person);
+            }
+        }
+
+        return result;
+    }
+
+    // TODO 3.15: Pridajte metódu Sort(), ktorá utriedi osoby
+    // TODO 3.16: Otestujte metódu Sort() v programe
+
+
+    public void PrintToConsole()
+    {
+        foreach (var person in _people)
+        {
+            Console.WriteLine(person);
+        }
+    }
+}
---
+++ w5/Person.cs
@@ -0,0 +1,81 @@
+﻿namespace PeopleDatabaseLibrary;
+
+// TODO 3.14: Implementujte rozhranie IComparable explicitne a IComparable<Person> implicitne
+// Porovnajte priezviska, mena, pohlavie a datum narodenia. Metóda vracia 0, ak sa objekty rovnajú. V inom prípade kladné alebo záporné číslo - pozrite si dokumentáciu.
+public class Person
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+		
+	public string FullName => $"{FirstName} {LastName}";
+
+    // TODO 3.02: Pridajte metódu GetFullName(), ktorá bude vracať n-ticu (tuple) zloženú z mena a priezviska, pričom priezvisko bude s veľkými písmenami
+    // TODO 3.03: V konzolovej aplikácii použite túto metódu na získanie hodnôt a vypíšte najskôr priezvisko a potom meno osoby
+   
+
+    public DateTime? Birthdate { get; set; } // Je to rovnaké ako: public Nullable<DateTime> Birthdate { get; set; }
+		
+	public int Age
+	{
+		get
+		{
+			var birthday = Birthdate;
+			if (!birthday.HasValue)
+				return 0;		
+
+			var today = DateTime.Today;
+			var age = today.Year - birthday.Value.Year;
+			if (today.Month < birthday.Value.Month || today.Month == birthday.Value.Month && today.Day < birthday.Value.Day)
+				age--;
+
+			return age;
+		} 
+	}
+		
+	public Gender Gender { get; set; }
+
+    // TODO 3.04: Vytvorte dekonštruktor, ktorý vráti meno, priezvisko a pohlavie 
+    // TODO 3.05: Otestujte dekonštruktor v programe
+
+
+    // TODO 3.06: Vytvorte druhý preťažený dekonštruktor, ktorý vráti meno, priezvisko, dátum narodenia, pohlavie a vek
+    // TODO 3.07: Otestujte dekonštruktor v programe
+
+
+    public Person()
+	{
+		FirstName = string.Empty;
+		LastName = string.Empty;
+	}
+
+	// TODO 3.01: Prerobte konštruktor na jednoriadkový s použitím syntaxe n-tíc
+	public Person(string firstName, string lastName, DateTime? birthday, Gender gender = Gender.Unknown)
+	{
+		FirstName = firstName;
+		LastName = lastName;
+		Birthdate = birthday;
+		Gender = gender;
+	}
+
+	public override string ToString()
+	{
+		return $"{FirstName} {LastName}, {Birthdate:dd.MM.yyyy}, age: ({Age}), gender: {Gender}";
+	}
+
+	/// <inheritdoc />
+	public override bool Equals(object? obj)
+	{
+		return obj is Person person &&
+			   FirstName == person.FirstName &&
+			   LastName == person.LastName &&
+			   Birthdate == person.Birthdate &&
+			   Gender == person.Gender;
+	}
+
+	/// <inheritdoc />
+	public override int GetHashCode()
+	{
+		return HashCode.Combine(FirstName, LastName, Birthdate, Gender);
+	}
+}
